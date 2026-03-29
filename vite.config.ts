7bcd42182f2fb2f7cba99db83cf03bce8add71fd
Acmeh1 Asma Saibi <saibi.asma15@gmail.com>
2026-03-29 12:16:18 +0200

    fix: SPA routing for production

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/ – cache bust
 export default defineConfig(({ mode }) => ({
+  base: "./",
   server: {
     host: "::",
     port: 8080,
